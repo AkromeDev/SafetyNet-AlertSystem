e0158889195b3534f19c1a849ad11c4c1ad76717
j.c. <jcbackenddev@gmail.com>
2020-08-05 16:49:13 +0200

    PersonalInformation: Data entity class created.
    I created a data entit class "PersonalInformation" that will hold all
    the information about the habitants of the different areas.

---
+++ alertsystem/src/main/java/com/safetynet/alertsystem/PersonalInformation.java
@@ -0,0 +1,81 @@
+package com.safetynet.alertsystem;
+
+public class PersonalInformation {
+	
+	private String firstName;
+	private String lasttName;
+	private String adress;
+	private String city;
+	private String zip;
+	private String phone;
+	private String email;
+	
+	public PersonalInformation(String firstName, String lasttName, String adress, String city, String zip, String phone,
+			String email) {
+		super();
+		this.firstName = firstName;
+		this.lasttName = lasttName;
+		this.adress = adress;
+		this.city = city;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLasttName() {
+		return lasttName;
+	}
+
+	public void setLasttName(String lasttName) {
+		this.lasttName = lasttName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
