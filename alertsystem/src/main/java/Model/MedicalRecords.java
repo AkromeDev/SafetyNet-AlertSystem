02440699cc3f96b53f71069604a06e5282327f6a
j.c. <jcbackenddev@gmail.com>
2020-08-06 15:17:24 +0200

    Last (but not least) Data entity class for our app created

---
+++ alertsystem/src/main/java/Model/MedicalRecords.java
@@ -0,0 +1,60 @@
+package Model;
+
+public class MedicalRecords {
+	
+	private String firstName;
+	private String lasttName;
+	private String birthdate;
+	private String medications;
+	private int id;
+	
+	public MedicalRecords(String firstName, String lasttName, String birthdate, String medications, int id) {
+		super();
+		this.firstName = firstName;
+		this.lasttName = lasttName;
+		this.birthdate = birthdate;
+		this.medications = medications;
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLasttName() {
+		return lasttName;
+	}
+
+	public void setLasttName(String lasttName) {
+		this.lasttName = lasttName;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getMedications() {
+		return medications;
+	}
+
+	public void setMedications(String medications) {
+		this.medications = medications;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
