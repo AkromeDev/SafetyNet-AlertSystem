b9201d694d794c7e9ede7cf87281559e095d35cf
j.c. <jcbackenddev@gmail.com>
2020-08-06 16:35:38 +0200

    Log4j installed in resources/Pom/and first log in AlertsystemApplication

--- alertsystem/src/main/java/com/safetynet/alertsystem/AlertController.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/AlertController.java
@@ -1,10 +1,16 @@
 package com.safetynet.alertsystem;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,11 +21,13 @@ import Model.PersonalInformation;
 public class AlertController {
 	
 	private List<PersonalInformation> personalInfo = new ArrayList<PersonalInformation>();
+	private List<PersonalInformation> medicalRecords = new ArrayList<PersonalInformation>();
+	private List<PersonalInformation> firestations = new ArrayList<PersonalInformation>();
 	private static int index = 1;
 	
 	
-	@GetMapping("/alertsystem")
-	public ModelAndView getPerson() {
+	@GetMapping("/personalInfo")
+	public ModelAndView getPersonalInfo() {
 		
 		personalInfo.add(new PersonalInformation("","","","","","","", index++));
 		
@@ -30,6 +38,33 @@ public class AlertController {
 		model.put("personInfo", 1234);
 		
 		return new ModelAndView(viewName, model);
+	}
+	
+	public List<PersonalInformation> getJsonData() {
+		
+		// JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+		
+		try (FileReader reader = new FileReader("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json")) {
+			
+			// Read JSON file
+			Object obj = jsonParser.parse(reader);
+			
+			JSONArray personalInfoList = (JSONArray) obj;
+			System.out.println(personalInfoList);
+			
+		} catch (FileNotFoundException e) {
+			// TODO add a logger to all the exceptions with a good text location
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
--- alertsystem/src/main/java/com/safetynet/alertsystem/AlertsystemApplication.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/AlertsystemApplication.java
@@ -1,12 +1,17 @@
 package com.safetynet.alertsystem;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlertsystemApplication {
+	
+	private static final Logger logger = LogManager.getLogger("App");
 
 	public static void main(String[] args) {
+		logger.info("Initializing SafetyNet App");
 		SpringApplication.run(AlertsystemApplication.class, args);
 	}
 
