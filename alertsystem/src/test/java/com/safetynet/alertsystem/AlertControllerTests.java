935ea7cac44f478276c8c528a820530d0d5bde66
j.c. <jcbackenddev@gmail.com>
2020-08-08 14:27:05 +0200

    First unit testing implemented
    I began right away with the unit testing in order to know if the code is
    working well.

---
+++ alertsystem/src/test/java/com/safetynet/alertsystem/AlertControllerTests.java
@@ -0,0 +1,38 @@
+package com.safetynet.alertsystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AlertControllerTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		//ARRANGE
+		AlertController alert = new AlertController();
+		
+		//ACT & ASSERT
+		System.out.println(alert.getJsonData());
+		assertNotNull(alert.getJsonData());
+	}
+}
