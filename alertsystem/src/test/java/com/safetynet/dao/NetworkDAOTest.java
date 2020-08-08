6db801d15e6a03eebe29e8bfaba2eef0db4f741e
j.c. <jcbackenddev@gmail.com>
2020-08-08 16:03:28 +0200

    Unit Testing for the NetworkDAO class

---
+++ alertsystem/src/test/java/com/safetynet/dao/NetworkDAOTest.java
@@ -0,0 +1,40 @@
+package com.safetynet.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NetworkDAOTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() throws IOException {
+		NetworkDAO networkDao = new NetworkDAO();
+		
+		System.out.println(networkDao.request("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json"));
+		
+		assertNotNull(networkDao.request("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json"));
+	}
+	
+}
