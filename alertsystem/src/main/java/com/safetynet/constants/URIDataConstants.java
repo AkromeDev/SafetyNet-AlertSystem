5850de1700f8978ffc434071a49514940bab505b
j.c. <jcbackenddev@gmail.com>
2020-08-09 13:36:13 +0200

    Constant class created containing the link(s) for the needed data
    ... I know, it is not really elegant to do so, and I should make the
    link configurable and not hard code it in my application like present.
    This is though outside the scope of this project since OC just gave one
    link as a data resource and not multiple ones.

---
+++ alertsystem/src/main/java/com/safetynet/constants/URIDataConstants.java
@@ -0,0 +1,7 @@
+package com.safetynet.constants;
+
+public class URIDataConstants {
+
+	    public static final String JSON_LINK = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json";
+
+}
