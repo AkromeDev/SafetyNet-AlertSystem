80d74ec5932168d8b54dd0f3fcfaf0bdbd5b290d
j.c. <jcbackenddev@gmail.com>
2020-08-19 15:56:57 +0200

    I am back from holidays! I missed coding sooo much!
    Ok! I had some real problem witht he validation annotations! but I
    finally found the solution. I added the annotation @ModelAttribute to
    the controller method in order to implement the Data binding from MVC
    Spring.
    
    Also, my big mistake was in the HTML! HTML is so easy though, but I
    guess it is also easy to do a mistake. The span in which I added the
    Thymeleaf annotation th:errors was outside the input div! I fixed that
    for all the fields annnnnd Tada! Problem solved

--- alertsystem/src/main/java/Model/PersonalInformation.java
+++ alertsystem/src/main/java/Model/PersonalInformation.java
@@ -1,14 +1,36 @@
 package Model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class PersonalInformation {
 	
+	@Size(max= 20, message="If that's really the first name... I am sorry... please make it shorter")
+	@NotEmpty(message="We are saving life! we need a first name stupid pumkin")
 	private String firstName;
+	
+	@Size(max= 20, message="If that's really the last name... I am sorry... please make it shorter")
+	@NotEmpty(message="We are saving life! we need a last name retard Hill-Billy")
 	private String lastName;
+	
+	@NotEmpty(message="We are saving life! we need an adress dumnut")
 	private String address;
+	
+	@NotEmpty(message="man! fill it up")
 	private String city;
+	
+	@NotEmpty(message="EVERYTHING")
 	private String zip;
+	
+	@NotEmpty(message="IS")
 	private String phone;
+	
+	@NotEmpty(message="MANDATORY")
+	@Email
 	private String email;
+	
 	private Integer Id;
 	
 	public PersonalInformation(String firstName, String lastName, String address, String city, String zip, String phone,
--- alertsystem/src/main/java/com/safetynet/alertsystem/AlertController.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/AlertController.java
@@ -6,9 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.apache.http.client.ClientProtocolException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -78,7 +82,11 @@ public class AlertController {
 	}
 
 	@PostMapping("/addPersonForm")
-	public ModelAndView submitPersonInfoForm(PersonalInformation personInfo) {
+	public ModelAndView submitPersonInfoForm(@Valid @ModelAttribute("personInfo") PersonalInformation personInfo, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return new ModelAndView("addPersonForm");
+		}
 		
 		PersonalInformation existingPerson = findExistingPerson(personInfo.getId());
 		
