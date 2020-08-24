8459baf8702cba6796628be216810cabeea4289b
j.c. <jcbackenddev@gmail.com>
2020-08-24 10:32:37 +0200

    Unit test for Controller Refactored
    since we added dependency injection to our system, our testing was not
    working anymore. A @MockBean had to be added for AlertService (replacing
    the constructor).
    Also we now needed a real parameter to be implemented for
    TestSubmitPersonInfo(). I did that using .param.

--- alertsystem/src/test/java/com/safetynet/alertsystem/AlertControllerTests.java
+++ alertsystem/src/test/java/com/safetynet/alertsystem/AlertControllerTests.java
@@ -1,14 +1,22 @@
 package com.safetynet.alertsystem;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.safetynet.alertsystem.service.AlertService;
 
 @WebMvcTest
 @ExtendWith(SpringExtension.class)
@@ -17,7 +25,11 @@ class AlertControllerTests {
 	@Autowired
 	MockMvc mockMvc;
 	
+	@MockBean
+	AlertService alertService;
+	
 	@Test
+	@DisplayName("tests: if the controller send to the riht page/sucess status/Model size/view name")
 	public void testShowPersonInfoForm() throws Exception {
 		
 		mockMvc.perform(get("/addPersonForm"))
@@ -28,9 +40,17 @@ class AlertControllerTests {
 	}
 	
 	@Test
+	@DisplayName("tests: if the controller send to the riht page & sucess status")
 	public void testSubmitPersonInfoForm() throws Exception {
 		
-		mockMvc.perform(post("/addPersonForm"))
+		mockMvc.perform(post("/addPersonForm")
+				.param("firstName", "john")
+				.param("lastName", "something")
+				.param("address", "so long")
+				.param("city", "Why no many")
+				.param("zip", "parameters")
+				.param("phone", "000000000")
+				.param("email", "mybad@gmail.com"))
 		.andExpect(status().is3xxRedirection())
 		.andExpect(redirectedUrl("/person"));
 	}
