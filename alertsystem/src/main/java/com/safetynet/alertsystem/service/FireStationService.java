7b16418dfd3ab4ee4bb10b28b6c73c4ccb83ebcb
j.c. <jcbackenddev@gmail.com>
2020-09-23 13:44:06 +0200

    FireStationService new method deleteCityZipEmailFromJson()

--- alertsystem/src/main/java/com/safetynet/alertsystem/service/FireStationService.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/service/FireStationService.java
@@ -1,7 +1,9 @@
 package com.safetynet.alertsystem.service;
 
-import java.util.List;
+import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,28 @@ public class FireStationService {
 		this.fireStationRepo = fireStationRepo;
 	}
 
-	public List<PersonalInformation> getPeopleFromStation(String station) {
+	public ArrayList<PersonalInformation> getPeopleFromStation(Integer station) {
 		
 		return fireStationRepo.getPeopleFromStation(station);
+	}
+	
+	public JSONArray deleteCityZipEmailFromJson(JSONArray jsonArray) {
+		
+		// TODO ask Nick: Does this method belongs to a utility class, or is it good tp have it into your service class?
+		
+		JSONArray uptdatedJsonArray = new JSONArray();
+		
+        for (int i = 0; i < jsonArray.length(); i++) {
+        	
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            
+    		jsonObject.remove("city");
+    		jsonObject.remove("zip");
+    		jsonObject.remove("email");
+    		
+    		uptdatedJsonArray.put(jsonObject);
+        }
+	
+		return uptdatedJsonArray;
 	}
 }
