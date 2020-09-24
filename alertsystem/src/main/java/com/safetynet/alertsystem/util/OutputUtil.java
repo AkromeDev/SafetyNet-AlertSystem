38a9bd6da73631d1da31ab949bba36ab3f7a43ed
j.c. <jcbackenddev@gmail.com>
2020-09-24 13:08:58 +0200

    New util class
    the class was created in order to count the number of adults and
    children in an ArrayList<MedicalRecords> for the firestation endpoint!

---
+++ alertsystem/src/main/java/com/safetynet/alertsystem/util/OutputUtil.java
@@ -0,0 +1,61 @@
+package com.safetynet.alertsystem.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+import com.safetynet.alertsystem.model.MedicalRecords;
+
+public class OutputUtil {
+	
+	// TODO Ask Nick: Can a util class be a bean
+
+	public Integer findNumberOfChildren(ArrayList<MedicalRecords> medicalRecords) {
+		
+		Integer numberOfChildren = 0;
+		
+		for (MedicalRecords record: medicalRecords) {
+			
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/d/yyyy");
+			String date = record.getBirthdate();
+
+			//convert String to LocalDate
+			LocalDate localBirthDate = LocalDate.parse(date, formatter);
+			LocalDate today = LocalDate.now();
+			
+			long years = ChronoUnit.YEARS.between(localBirthDate, today);
+			
+			if (years < 18) {
+				numberOfChildren++;
+			}
+		}
+
+		return numberOfChildren;
+	}
+
+	public Integer findNumberOfAdults(ArrayList<MedicalRecords> medicalRecords) {
+		
+		Integer numberOfAdults = 0;
+		
+		for (MedicalRecords record: medicalRecords) {
+			
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/d/yyyy");
+			String date = record.getBirthdate();
+
+			//convert String to LocalDate
+			LocalDate localBirthDate = LocalDate.parse(date, formatter);
+			
+			
+			LocalDate today = LocalDate.now();
+			long years = ChronoUnit.YEARS.between(localBirthDate, today);
+			
+			if (years >= 18) {
+				numberOfAdults++;
+			}
+		}
+
+		return numberOfAdults;
+	}
+
+}
