9e5b19818a76e41a52ac92e467c2bb9bf9e42a77
j.c. <jcbackenddev@gmail.com>
2020-09-28 21:27:07 +0200

    New test class for the custom error controller

---
+++ alertsystem/src/test/java/com/safetynet/alertsystem/controller/CustomErrorControllerTests.java
@@ -0,0 +1,44 @@
+package com.safetynet.alertsystem.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomErrorControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	
+	
+	@Test
+	@DisplayName("tests /person: if the controller send to the riht page/sucess status/Model size/view name")
+	public void testGetPersonalInfo() throws Exception {
+		
+		mockMvc.perform(get("/error"))
+		.andExpect(status().is2xxSuccessful())
+		.andExpect(view().name("error"))
+		.andExpect(model().size(0))
+		.andExpect(content().string(containsString("This is embarrassing!")));
+	}
+	
+}
