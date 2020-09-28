bc53c80c8f3091204584ac41ed0d9388e0b321eb
j.c. <jcbackenddev@gmail.com>
2020-09-28 21:03:24 +0200

    New Test for the /person endpoint

--- alertsystem/src/test/java/com/safetynet/alertsystem/controller/AlertControllerTests.java
+++ alertsystem/src/test/java/com/safetynet/alertsystem/controller/AlertControllerTests.java
@@ -1,32 +1,52 @@
 package com.safetynet.alertsystem.controller;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.safetynet.alertsystem.service.AlertService;
-
-@WebMvcTest
-@ExtendWith(SpringExtension.class)
-class AlertControllerTests {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AlertControllerTests {
 
 	@Autowired
-	MockMvc mockMvc;
+	private MockMvc mockMvc;
+
+	@Test
+	@DisplayName("tests: if we have connection and if the homepage is correctly loaded")
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Welcome to the Alert System application from SafetyNet")));
+	}
 	
-	@MockBean
-	AlertService alertService;
+	
+	@Test
+	@DisplayName("TODO")
+	public void testGetPersonalInfo() throws Exception {
+		
+		mockMvc.perform(get("/person"))
+		.andExpect(status().is2xxSuccessful())
+		.andExpect(view().name("person"))
+		.andExpect(model().size(2))
+		.andExpect(model().attributeExists("person"))
+		.andExpect(model().attributeExists("numberToSave"));
+	}
 	
 	@Test
 	@DisplayName("tests: if the controller send to the riht page/sucess status/Model size/view name")
@@ -40,7 +60,7 @@ class AlertControllerTests {
 	}
 	
 	@Test
-	@DisplayName("tests: if the controller send to the riht page & sucess status")
+	@DisplayName("tests: if the controller send to the right page & sucess status")
 	public void testSubmitPersonInfoForm() throws Exception {
 		
 		mockMvc.perform(post("/addPersonForm")
@@ -51,8 +71,8 @@ class AlertControllerTests {
 				.param("zip", "parameters")
 				.param("phone", "000000000")
 				.param("email", "mybad@gmail.com"))
-		.andExpect(status().is3xxRedirection())
-		.andExpect(redirectedUrl("/person"));
+		.andExpect(status().is3xxRedirection());
+//		.andExpect(forwardedUrl("/person"));
 	}
 
 }
