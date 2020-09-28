ebe8455c802d53a6baf871578a5a65fd2b9a9823
j.c. <jcbackenddev@gmail.com>
2020-09-28 14:11:04 +0200

    Changed location from Controller test class

--- alertsystem/src/test/java/com/safetynet/alertsystem/controller/AlertControllerTests.java
+++ alertsystem/src/test/java/com/safetynet/alertsystem/controller/AlertControllerTests.java
@@ -1,4 +1,4 @@
-package com.safetynet.alertsystem;
+package com.safetynet.alertsystem.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
