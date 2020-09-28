f5947f1c953f1d117aad04cc66f800608978f38b
j.c. <jcbackenddev@gmail.com>
2020-09-28 14:44:30 +0200

    New Test Unit Class
    We now are testing if the application is online and that all the
    controllers are begin created

--- alertsystem/src/test/java/com/safetynet/alertsystem/AlertsystemApplicationTests.java
+++ alertsystem/src/test/java/com/safetynet/alertsystem/AlertsystemApplicationTests.java
@@ -1,13 +1,49 @@
 package com.safetynet.alertsystem;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.safetynet.alertsystem.controller.AlertController;
+import com.safetynet.alertsystem.controller.CustomErrorController;
+import com.safetynet.alertsystem.controller.FireStationController;
+import com.safetynet.alertsystem.controller.PeopleController;
 
 @SpringBootTest
 class AlertsystemApplicationTests {
 
+	@Autowired
+	private AlertController alertController;
+	
+	@Autowired
+	private CustomErrorController errorController;
+	
+	@Autowired
+	private FireStationController fireController;
+	
+	@Autowired
+	private PeopleController peopleController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoadsAlert() throws Exception {
+		assertThat(alertController).isNotNull();
+	}
+	
+	@Test
+	public void contextLoadsError() throws Exception {
+		assertThat(errorController).isNotNull();
+	}
+	
+	@Test
+	public void contextLoadsFire() throws Exception {
+		assertThat(fireController).isNotNull();
+	}
+	
+	@Test
+	public void contextLoadsPeople() throws Exception {
+		assertThat(peopleController).isNotNull();
 	}
 
 }
