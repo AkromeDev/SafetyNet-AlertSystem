bbdf7cde3fd194d7eade163b84e3b8301f9bac05
jcbackenddev <jcbackenddev@gmail.com>
2020-10-09 14:55:29 +0200

    FireRepo Tests

--- alertsystem/src/test/java/com/safetynet/alertsystem/repository/FireStationRepositoryTest.java
+++ alertsystem/src/test/java/com/safetynet/alertsystem/repository/FireStationRepositoryTest.java
@@ -9,17 +9,25 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.safetynet.alertsystem.dao.ModelDAO;
 import com.safetynet.alertsystem.model.PersonalInformation;
 
 class FireStationRepositoryTest {
 
 	static private FireStationRepository fireRepo;
 	static private ArrayList<PersonalInformation> personalList;
+	static private ArrayList<Integer> list = new ArrayList<Integer>();
+	
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		ModelDAO.fetchPersonalInformationFromJson();
+		
+		list.add(1);
+		list.add(2);
 	}
 
 	@AfterAll
@@ -34,19 +42,23 @@ class FireStationRepositoryTest {
 	void tearDown() throws Exception {
 	}
 
-//	@Test
-//	void getPeopleFromStationTest() {
-//		personalList = fireRepo.getPeopleFromStation(1);
-//		
-//		PersonalInformation personalInfo = personalList.get(0);
-//		
-//		assertEquals(personalInfo.getAddress(), "1509 Culver St");
-//	}
-//	
-//	@Test
-//	void findFireStationByNumberTest() {
-//		
-//		assertNotNull(fireRepo.findFireStationAreasByNumber(1));
-//	}
+	@Test
+	@DisplayName("Tests if the first gotten from the person is the right one and if the list has the right size")
+	void getPeopleFromStationTest() {
+		personalList = fireRepo.getPeopleFromStation(list);
+		
+		System.out.println(personalList);
+		
+		PersonalInformation personalInfo = personalList.get(0);
+		
+		assertEquals(personalInfo.getAddress(), "1509 Culver St");
+		assert(personalList.size() > 5);
+	}
+	
+	@Test
+	void findFireStationByNumberTest() {
+		
+		assertNotNull(fireRepo.findFireStationAreasByNumber(list));
+	}
 
 }
