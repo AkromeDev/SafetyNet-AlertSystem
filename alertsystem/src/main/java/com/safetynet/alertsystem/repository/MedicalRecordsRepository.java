827cf8b5e14d29c9899ab04ceb61678604379e8b
jcbackenddev <jcbackenddev@gmail.com>
2020-10-16 12:48:42 +0200

    findAndUpdateMedicalRecordbyNames() added
    This method is used for the put option from the controller

--- alertsystem/src/main/java/com/safetynet/alertsystem/repository/MedicalRecordsRepository.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/repository/MedicalRecordsRepository.java
@@ -1,14 +1,19 @@
 package com.safetynet.alertsystem.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.safetynet.alertsystem.dao.ModelDAO;
+import com.safetynet.alertsystem.model.HabitantAndRecords;
 import com.safetynet.alertsystem.model.MedicalRecords;
 
 @Repository
 public class MedicalRecordsRepository {
 	
 	ModelDAO modelDAO = new ModelDAO();
+	MedicalRecordsRepository medRecRepo = new MedicalRecordsRepository();
 
 	public MedicalRecords saveMedicalRecord(MedicalRecords mediRecord) {
 		
@@ -16,7 +21,26 @@ public class MedicalRecordsRepository {
 	}
 
 	public MedicalRecords putMedicalRecord(MedicalRecords mediRecord) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return medRecRepo.findAndUpdateMedicalRecordbyNames(mediRecord);
+	}
+	
+	public MedicalRecords findAndUpdateMedicalRecordbyNames(MedicalRecords mediRecord) {
+		
+		MedicalRecords chosenOne = new MedicalRecords();
+		
+		for (MedicalRecords potentialOne: modelDAO.getMedicalRecordsFromJson()) {
+				if (potentialOne.getFirstName().equals(mediRecord.getFirstName()) && potentialOne.getLastName().contentEquals(mediRecord.getLastName())) {
+					potentialOne.setAllergies(mediRecord.getAllergies());
+					potentialOne.setMedications(mediRecord.getMedications());
+					potentialOne.setBirthdate(mediRecord.getBirthdate());
+				}
+			}
+		return chosenOne;
+	}
+
+	public MedicalRecords deleteMedicalRecord(String firstName, String lastName) {
+		
+		return modelDAO.deleteMedicalRecord(firstName, lastName);
 	}
 }
