2b90353b5f53111ade3eb52b0afd33dc6bd12b42
jcbackenddev <jcbackenddev@gmail.com>
2020-10-19 20:20:50 +0200

    findNewId() added the method was necessary to add a non existing id to the objects being created

--- alertsystem/src/main/java/com/safetynet/alertsystem/repository/MedicalRecordsRepository.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/repository/MedicalRecordsRepository.java
@@ -9,34 +9,33 @@ import com.safetynet.alertsystem.model.MedicalRecords;
 public class MedicalRecordsRepository {
 	
 	ModelDAO modelDAO = new ModelDAO();
-	MedicalRecordsRepository medRecRepo = new MedicalRecordsRepository();
 
 	public MedicalRecords saveMedicalRecord(MedicalRecords mediRecord) {
 		
-		return modelDAO.addMedicalRecordList(mediRecord);
+		mediRecord.setId(findNewId(mediRecord));
+		
+		return modelDAO.addMedicalRecordToList(mediRecord);
 	}
 
 	public MedicalRecords putMedicalRecord(MedicalRecords mediRecord) {
 
-		return medRecRepo.findAndUpdateMedicalRecordbyNames(mediRecord);
+		return modelDAO.findAndUpdateMedicalRecordbyNames(mediRecord);
 	}
 	
-	public MedicalRecords findAndUpdateMedicalRecordbyNames(MedicalRecords mediRecord) {
-		
-		MedicalRecords chosenOne = new MedicalRecords();
-		
-		for (MedicalRecords potentialOne: modelDAO.getMedicalRecordsFromJson()) {
-				if (potentialOne.getFirstName().equals(mediRecord.getFirstName()) && potentialOne.getLastName().contentEquals(mediRecord.getLastName())) {
-					potentialOne.setAllergies(mediRecord.getAllergies());
-					potentialOne.setMedications(mediRecord.getMedications());
-					potentialOne.setBirthdate(mediRecord.getBirthdate());
-				}
-			}
-		return chosenOne;
-	}
-
 	public MedicalRecords deleteMedicalRecord(String firstName, String lastName) {
 		
 		return modelDAO.deleteMedicalRecord(firstName, lastName);
 	}
-}
+	
+	public Integer findNewId(MedicalRecords mediRecord) {
+		Integer id = 0;
+		
+		for (MedicalRecords record: modelDAO.getMedicalRecordsFromJson()) {
+			if (record.getId() > id) {
+				id = record.getId();
+			}
+		}
+	
+		return id + 1;
+		}
+	}
