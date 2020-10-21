b7610c33e83df6ca1fd7b6ca7e0703116b4f05b3
jcbackenddev <jcbackenddev@gmail.com>
2020-10-21 23:23:56 +0200

    Renaming + POST / PUT / DELETE methods created

---
+++ alertsystem/src/main/java/com/safetynet/alertsystem/service/RestService.java
@@ -0,0 +1,51 @@
+package com.safetynet.alertsystem.service;
+
+import org.json.JSONTokener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.safetynet.alertsystem.model.MedicalRecords;
+import com.safetynet.alertsystem.model.PersonalInformation;
+import com.safetynet.alertsystem.repository.RestRepository;
+
+@Service
+public class RestService {
+
+	RestRepository restRepo;
+	
+	@Autowired
+	public RestService(RestRepository restRepo) {
+		super();
+		this.restRepo = restRepo;
+	}
+
+	public MedicalRecords saveMedicalRecord(MedicalRecords mediRecord) {
+
+		return restRepo.saveMedicalRecord(mediRecord);
+	}
+
+	public MedicalRecords putMedicalRecord(MedicalRecords mediRecord) {
+		
+		return restRepo.putMedicalRecord(mediRecord);
+	}
+
+	public MedicalRecords deleteMedicalRecord(String firstName, String lastName) {
+	
+		return restRepo.deleteMedicalRecord(firstName, lastName);
+	}
+	
+	public PersonalInformation savePerson(PersonalInformation person) {
+
+		return restRepo.savePerson(person);
+	}
+	
+	public PersonalInformation putPersonalInformation(PersonalInformation person) {
+		
+		return restRepo.putPersonalInformation(person);
+	}
+
+	public PersonalInformation deletePerson(String firstName, String lastName) {
+
+		return restRepo.deletePerson(firstName, lastName);
+	}
+}
