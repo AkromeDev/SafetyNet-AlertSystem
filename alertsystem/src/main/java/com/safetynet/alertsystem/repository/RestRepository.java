690344e4a8656dadf23fe43aef31be1030b0c8d4
jcbackenddev <jcbackenddev@gmail.com>
2020-10-21 23:24:22 +0200

    Renaming + POST /PUT / DELETE method created

--- alertsystem/src/main/java/com/safetynet/alertsystem/repository/RestRepository.java
+++ alertsystem/src/main/java/com/safetynet/alertsystem/repository/RestRepository.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Repository;
 
 import com.safetynet.alertsystem.dao.ModelDAO;
 import com.safetynet.alertsystem.model.MedicalRecords;
+import com.safetynet.alertsystem.model.PersonalInformation;
 
 @Repository
-public class MedicalRecordsRepository {
+public class RestRepository {
 	
 	ModelDAO modelDAO = new ModelDAO();
 
 	public MedicalRecords saveMedicalRecord(MedicalRecords mediRecord) {
 		
-		mediRecord.setId(findNewId(mediRecord));
+		mediRecord.setId(findMediRecoNewId());
 		
 		return modelDAO.addMedicalRecordToList(mediRecord);
 	}
@@ -27,7 +28,7 @@ public class MedicalRecordsRepository {
 		return modelDAO.deleteMedicalRecord(firstName, lastName);
 	}
 	
-	public Integer findNewId(MedicalRecords mediRecord) {
+	public Integer findMediRecoNewId() {
 		Integer id = 0;
 		
 		for (MedicalRecords record: modelDAO.getMedicalRecordsFromJson()) {
@@ -37,5 +38,35 @@ public class MedicalRecordsRepository {
 		}
 	
 		return id + 1;
+	}
+	
+	public PersonalInformation savePerson(PersonalInformation person) {
+		
+		person.setId(findPersonNewId());
+		
+		return modelDAO.addPersonToList(person);
+	}
+	
+	public Integer findPersonNewId() {
+		Integer id = 0;
+		
+		for (PersonalInformation human: modelDAO.getPeopleFromJson()) {
+			if (human.getId() > id) {
+				id = human.getId();
+			}
 		}
+	
+		return id + 1;
+	}
+
+	public PersonalInformation putPersonalInformation(PersonalInformation person) {
+		
+		return modelDAO.findAndUpdatePersonalInformation(person);
+	}
+
+	public PersonalInformation deletePerson(String firstName, String lastName) {
+
+		return modelDAO.deletePerson(firstName, lastName);
+	}
+	
 	}
